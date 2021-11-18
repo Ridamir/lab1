e0bc0f665c822a836a2aaa3245df8c9c70e26201
ridamir <amirmohammedrida@gmail.com>
2021-11-18 22:18:33 +0100

    3th release -- implementing display to tft LCD scree

--- lab1.cpp
+++ lab1.cpp
@@ -1,46 +1,49 @@
 #include <stdio.h>
 #include "tpl_os.h"
 #include "mcp23s17.h"
-
+#include "tft.h"
 
 FUNC(int, OS_APPL_CODE) main(void)
 {
     initCoroBoard();
     StartOS(OSDEFAULTAPPMODE);
+    setupTimer();
+    Tft.setTextCursor(0,0); 
     return 0;
 }
 
 
 TASK(task1)
 {
-    printf("task1 is running\n");    
+    uint32 time1=0;
+    time1=getTimerValue();
+    Tft.println("task1 is running at");
+    Tft.println(time1);
     ioExt.digitalWrite(mcp23s17::PORTA, 0, 1);
-    printf("task1 is running, LED A0 is turned on\n");
-
-    printf("call to ActivateTask() to task2 will be performed\n");
     ActivateTask(task2);
-    printf("call to ActivateTask() to task2 is performed\n");
 
-    printf("call to ActivateTask() to task3 will be performed\n");
     ActivateTask(task3);
-    printf("call to ActivateTask() to task3 is performed\n");
-    
+
     TerminateTask();
 }
 
 TASK(task2)
 {
-    printf("task2 is running\n");        
+    uint32 time2=0;
+    time2=getTimerValue();
+    Tft.println("task2 is running at");
+    Tft.println(time2);
     ioExt.digitalWrite(mcp23s17::PORTA, 2, 1);
-    printf("task2 is running, LED A2 is turned on\n");
     TerminateTask();
 }
 
 TASK(task3)
 {
-    printf("task3 is running\n");    
+    uint32 time3=0;
+    time3=getTimerValue();
+    Tft.println("task3 is running at");
+    Tft.println(time3);
     ioExt.digitalWrite(mcp23s17::PORTA, 3, 1);
-    printf("task3 is running, LED A3 is turned on\n");
     TerminateTask();
 }
 
